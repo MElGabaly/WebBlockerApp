f7884782ccddada724e82c9c000a6aee2de64481
Mo <Gabalyma@gmail.com>
2020-01-07 21:59:24 -0800

    Finished Program but need to make it work automaticlly

--- blocker.py
+++ blocker.py
@@ -2,14 +2,15 @@ import time
 from datetime import datetime as dt
 
 # setting up the intial variables
-host_path = "hosts"
+host_path = "/private/etc/hosts"
 redirect = "127.0.0.1"
+#
 website_list = ["www.facebook.com", "facebook.com",
                 "www.instagram.com", "instagram.com"]
 
 while True:
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 21):
-        print("Working Hours")
+        print("Working Hours...")
         with open(host_path, 'r+') as file:
             content = file.read()
             for website in website_list:
@@ -18,5 +19,16 @@ while True:
                 else:
                     file.write(redirect + " " + website + "\n")
     else:
-        print("Fun Hours")
+        print("Fun Hours...")
+        with open(host_path, 'r+') as file:
+            # read line by line
+            content = file.readlines()
+            # to start the file from the beginning
+            file.seek(0)
+            for line in content:
+                # If any of the above is True you get the expression evaluated to True. In this case none of them is True, so you get False.
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            # to delete anything below that point
+            file.truncate()
     time.sleep(5)
